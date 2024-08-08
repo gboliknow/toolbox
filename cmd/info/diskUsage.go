0fa244a7c6db0b3dac6e949ae90e85d3038f1867
gbolahan <adekoyegbolahan001@gmail.com>
2024-08-08 16:26:14 +0100

    :art: Partly complex cli for ping and  diskSpace

---
+++ cmd/info/diskUsage.go
@@ -0,0 +1,42 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
+package info
+
+import (
+	"fmt"
+	"github.com/ricochet2200/go-disk-usage/du"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// diskUsageCmd represents the diskUsage command
+var diskUsageCmd = &cobra.Command{
+	Use:   "diskUsage",
+	Short: "Disk Usgae of the current directory",
+	Long: ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		defaultDirectory := "."
+		if dir := viper.GetViper().GetString("cmd.info.diskUsage.defaultDirectory"); dir != "" {
+			defaultDirectory = dir
+		}
+		usage := du.NewDiskUsage(defaultDirectory)
+
+		fmt.Printf("Free disk space:%d in directory %s\n", usage.Free(), defaultDirectory)
+	},
+}
+
+func init() {
+	InfoCmd.AddCommand(diskUsageCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// diskUsageCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// diskUsageCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cmd/net/net.go
@@ -0,0 +1,33 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
+package net
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// NetCmd represents the net command
+var NetCmd = &cobra.Command{
+	Use:   "net",
+	Short: "A brief description of your command",
+	Long: ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
+
+func init() {
+	//rootCmd.AddCommand(NetCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// netCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// netCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
